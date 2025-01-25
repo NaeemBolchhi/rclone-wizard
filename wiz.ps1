9558f9826497cbe8b268cbe9a9dd9f8e95aba225
NaeemBolchhi <naeembolchhi.dev@gmail.com>
2025-01-25 23:40:23 +0600

    wiz icon

--- wiz.ps1
+++ wiz.ps1
@@ -4,6 +4,7 @@
 $DownloadBAT = 'https://naeembolchhi.github.io/rclone-wizard/wiz-20250025233256.bat'
 $DownloadICON1 = 'https://naeembolchhi.github.io/rclone-wizard/mount.ico'
 $DownloadICON2 = 'https://naeembolchhi.github.io/rclone-wizard/unmount.ico'
+$DownloadICON3 = 'https://naeembolchhi.github.io/rclone-wizard/wiz.ico'
 
 $folderPath = "$env:ProgramData\TmFlZW1Cb2xjaGhp"
 if (-not (Test-Path -Path $folderPath)) {
@@ -13,6 +14,7 @@ if (-not (Test-Path -Path $folderPath)) {
 $FilePath = "$folderPath\wiz.bat"
 $IconPath1 = "$folderPath\mount.ico"
 $IconPath2 = "$folderPath\unmount.ico"
+$IconPath3 = "$folderPath\wiz.ico"
 
 if (Test-Path $FilePath) {
     $item = Get-Item -LiteralPath $FilePath
@@ -26,11 +28,16 @@ if (Test-Path $IconPath2) {
     $item = Get-Item -LiteralPath $IconPath2
     $item.Delete()
 }
+if (Test-Path $IconPath3) {
+    $item = Get-Item -LiteralPath $IconPath3
+    $item.Delete()
+}
 
 try {
     Invoke-WebRequest -Uri $DownloadBAT -UseBasicParsing -OutFile $FilePath
 	Invoke-WebRequest -Uri $DownloadICON1 -UseBasicParsing -OutFile $IconPath1
 	Invoke-WebRequest -Uri $DownloadICON2 -UseBasicParsing -OutFile $IconPath2
+    Invoke-WebRequest -Uri $DownloadICON3 -UseBasicParsing -OutFile $IconPath3
 } catch {
     Write-Error $_
 	Return
